7ffed2c17229c62200fcb67843de42a7002b4a7b
Parsa Yazdani <3parsa3@gmail.com>
2024-05-30 22:21:53 +1000

    Node discovery and announce

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
+const crypto = require('crypto');
 
 /* TODO ////////////////////////////////////////
-- Node announce
-- Node discovery
 - Node health check
 - File checksum validation
 - File content length validation
@@ -15,7 +14,7 @@ const fetch = require('node-fetch');
 // CONFIG /////////////////////////////////////
 const PORT = 3000;
 const HOSTNAME = '127.0.0.1';
-const MAX_STORAGE = 10 * 1024 * 1024 * 1024; // 10GB
+const MAX_STORAGE = 100 * 1024 * 1024 * 1024; // 100GB
 const PERMA_FILES = []; // File hashes to never delete when storage limit is reached
 const BURN_RATE = 0.1; // Percentage of files to purge when storage limit is reached
 // CONFIG /////////////////////////////////////
@@ -48,9 +47,28 @@ const server = http.createServer(async (req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/html', 'Cache-Control': 'public, max-age=604800' });
         fs.createReadStream('index.html').pipe(res);
 
-    }else if (req.url === '/nodes'){
+    }else if(req.url === '/nodes'){
         res.writeHead(200, { 'Content-Type': 'application/json', 'Cache-Control': 'public, max-age=3600'});
         fs.createReadStream(NODES_PATH).pipe(res);
+
+    }else if(req.url.startsWith('/announce')){
+        const params = Object.fromEntries(new URLSearchParams(req.url.split('?')[1]));
+        const host = params['host'];
+        if(!host) return res.end('Invalid request\n');
+        
+        const response = await fetch(`${isIp(host) ? 'http' : 'https'}://${host}/download/04aa07009174edc6f03224f003a435bcdc9033d2c52348f3a35fbb342ea82f6f/c8fcb43d6e46`);
+        const arrayBuffer = await response.arrayBuffer();
+        const hashBuffer = await crypto.subtle.digest('SHA-256', arrayBuffer);
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hash = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        if(hash === '04aa07009174edc6f03224f003a435bcdc9033d2c52348f3a35fbb342ea82f6f'){
+            const nodes = JSON.parse(fs.readFileSync(NODES_PATH));
+            if(!nodes.find(node => node.host === host)){
+                nodes.push({host, http: true, dns: false, cf: false});
+                fs.writeFileSync(NODES_PATH, JSON.stringify(nodes));
+            }
+            res.end('Announced\n');
+        }else res.end('Invalid request\n');
 
     }else if(req.url.startsWith('/download/')){
         const fileHash = req.url.split('/')[2];
@@ -140,6 +158,30 @@ server.listen(PORT, HOSTNAME, async () => {
         fs.writeFileSync(NODES_PATH, JSON.stringify(nodes));
     }
 
+    // Call all nodes and pull their /nodes
+    for(const node of nodes){
+        if(node.http){
+            if(node.host === `${HOSTNAME}:${PORT}`) continue;
+            const response = await fetch(`${isIp(node.host) ? 'http' : 'https'}://${node.host}/nodes`);
+            if(response.status === 200){
+                const remoteNodes = await response.json();
+                for(const remoteNode of remoteNodes){
+                    if(!nodes.find(node => node.host === remoteNode.host)){
+                        const response = await fetch(`${isIp(remoteNode.host) ? 'http' : 'https'}://${remoteNode.host}/download/04aa07009174edc6f03224f003a435bcdc9033d2c52348f3a35fbb342ea82f6f/c8fcb43d6e46`);
+                        const arrayBuffer = await response.arrayBuffer();
+                        const hashBuffer = await crypto.subtle.digest('SHA-256', arrayBuffer);
+                        const hashArray = Array.from(new Uint8Array(hashBuffer));
+                        const hash = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+                        if(hash === '04aa07009174edc6f03224f003a435bcdc9033d2c52348f3a35fbb342ea82f6f'){
+                            await fetch(`${isIp(remoteNode.host) ? 'http' : 'https'}://${remoteNode.host}/announce?host=${HOSTNAME + (PORT != 80 ? `:${PORT}` : '')}`);
+                            nodes.push(remoteNode);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     // Calculate used storage
     const filesPath = path.join(__dirname, 'files');
     if(fs.existsSync(filesPath)){
@@ -152,5 +194,16 @@ server.listen(PORT, HOSTNAME, async () => {
     console.log(`Files dir size: ${usedStorage} bytes`);
 
     await fetch(`${isIp(HOSTNAME) ? 'http' : 'https'}://${HOSTNAME + (PORT != 80 ? `:${PORT}` : '')}/download/04aa07009174edc6f03224f003a435bcdc9033d2c52348f3a35fbb342ea82f6f/c8fcb43d6e46`);
-    if(!fs.existsSync(path.join(__dirname, 'files', '04aa07009174edc6f03224f003a435bcdc9033d2c52348f3a35fbb342ea82f6f'))) console.error('Download test failed')
+    if(!fs.existsSync(path.join(__dirname, 'files', '04aa07009174edc6f03224f003a435bcdc9033d2c52348f3a35fbb342ea82f6f')))
+        console.error('Download test failed')
+    else{
+        console.log('Announcing to nodes');
+        const nodes = JSON.parse(fs.readFileSync(NODES_PATH));
+        for(const node of nodes){
+            if(node.http){
+                // if(node.host === `${HOSTNAME}:${PORT}`) continue;
+                await fetch(`${isIp(node.host) ? 'http' : 'https'}://${node.host}/announce?host=${HOSTNAME + (PORT != 80 ? `:${PORT}` : '')}`);
+            }
+        }
+    }
 });
