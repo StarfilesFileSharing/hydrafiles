4667c09c4946330d905307e9a13811b807b6e51a
Parsa Yazdani (Quix) <parsa@yazdani.au>
2024-10-08 06:22:35 +1100

    Re-enable hash verification

--- src/nodes.ts
+++ src/nodes.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import CONFIG from './config'
-import { promiseWithTimeout, hasSufficientMemory, interfere, promiseWrapper } from './utils'
+import { promiseWithTimeout, hasSufficientMemory, interfere, promiseWrapper, hashStream } from './utils'
 import FileHandler from './fileHandler'
 
 export interface Node { host: string, http: boolean, dns: boolean, cf: boolean, hits: number, rejects: number, bytes: number, duration: number, status?: boolean }
@@ -53,9 +53,9 @@ export default class Nodes {
       console.log(`  ${hash}  Downloading from ${node.host}`)
       const response = await promiseWithTimeout(fetch(`${node.host}/download/${hash}`), CONFIG.timeout)
       console.log('hashing')
-      // const verifiedHash = await hashStream(response.body) // todo: causes OOM
+      const verifiedHash = await hashStream(response.body) // todo: causes OOM
       console.log('Done hashing')
-      // if (hash !== verifiedHash) return false
+      if (hash !== verifiedHash) return false
 
       file.set('name', String(response.headers.get('Content-Disposition')?.split('=')[1].replace(/"/g, '')))
       file.save().catch(console.error)
