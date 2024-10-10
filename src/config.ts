bb780650e34b2df2764be1df6bba270e829ed7bc
Parsa Yazdani (Quix) <parsa@yazdani.au>
2024-10-11 07:37:06 +1100

    Fallback to default config in-case node doesn't have all config set

--- src/config.ts
+++ src/config.ts
@@ -30,29 +30,30 @@ const DIRNAME = path.resolve()
 
 if (!fs.existsSync(path.join(DIRNAME, 'config.json'))) fs.copyFileSync(path.join(DIRNAME, 'config.default.json'), path.join(DIRNAME, 'config.json'))
 const config: Config = JSON.parse(fs.readFileSync(path.join(DIRNAME, 'config.json')).toString())
+const defaultConfig: Config = JSON.parse(fs.readFileSync(path.join(DIRNAME, 'config.default.json')).toString())
 
 const CONFIG: Config = {
-  port: config.port,
-  hostname: config.hostname,
-  max_storage: config.max_storage,
-  perma_files: config.perma_files,
-  burn_rate: config.burn_rate,
-  metadata_endpoint: config.metadata_endpoint,
-  bootstrap_nodes: config.bootstrap_nodes,
-  public_hostname: config.public_hostname,
-  prefer_node: config.prefer_node,
-  max_concurrent_nodes: config.max_concurrent_nodes,
-  upload_secret: config.upload_secret,
-  s3_access_key_id: config.s3_access_key_id,
-  s3_secret_access_key: config.s3_secret_access_key,
-  s3_endpoint: config.s3_endpoint,
-  cache_s3: config.cache_s3,
-  memory_threshold: config.memory_threshold,
-  memory_threshold_reached_wait: config.memory_threshold_reached_wait,
-  timeout: config.timeout,
-  log_level: config.log_level,
-  summary_speed: config.summary_speed,
-  compare_speed: config.compare_speed
+  port: config.port ?? defaultConfig.port,
+  hostname: config.hostname ?? defaultConfig.hostname,
+  max_storage: config.max_storage ?? defaultConfig.max_storage,
+  perma_files: config.perma_files ?? defaultConfig.perma_files,
+  burn_rate: config.burn_rate ?? defaultConfig.burn_rate,
+  metadata_endpoint: config.metadata_endpoint ?? defaultConfig.metadata_endpoint,
+  bootstrap_nodes: config.bootstrap_nodes ?? defaultConfig.bootstrap_nodes,
+  public_hostname: config.public_hostname ?? defaultConfig.public_hostname,
+  prefer_node: config.prefer_node ?? defaultConfig.prefer_node,
+  max_concurrent_nodes: config.max_concurrent_nodes ?? defaultConfig.max_concurrent_nodes,
+  upload_secret: config.upload_secret ?? defaultConfig.upload_secret,
+  s3_access_key_id: config.s3_access_key_id ?? defaultConfig.s3_access_key_id,
+  s3_secret_access_key: config.s3_secret_access_key ?? defaultConfig.s3_secret_access_key,
+  s3_endpoint: config.s3_endpoint ?? defaultConfig.s3_endpoint,
+  cache_s3: config.cache_s3 ?? defaultConfig.cache_s3,
+  memory_threshold: config.memory_threshold ?? defaultConfig.memory_threshold,
+  memory_threshold_reached_wait: config.memory_threshold_reached_wait ?? defaultConfig.memory_threshold_reached_wait,
+  timeout: config.timeout ?? defaultConfig.timeout,
+  log_level: config.log_level ?? defaultConfig.log_level,
+  summary_speed: config.summary_speed ?? defaultConfig.summary_speed,
+  compare_speed: config.compare_speed ?? defaultConfig.compare_speed
 }
 
 export default CONFIG
