638d0de358f408016bf091f62202be2afe863114
Parsa Yazdani (Quix) <parsa@yazdani.au>
2024-10-11 05:01:02 +1100

    Cache /files endpoint

--- src/index.ts
+++ src/index.ts
@@ -222,6 +222,7 @@ const handleRequest = async (req: http.IncomingMessage, res: http.ServerResponse
         res.end('200 OK\n')
       })
     } else if (req.url === '/files') {
+      res.writeHead(200, { 'Content-Type': 'application/json', 'Cache-Control': 'public, max-age=300' })
       const rows = (await FileModel.findAll()).map((row) => {
         const { hash, infohash, id, name, size } = row.dataValues
         return { hash, infohash, id, name, size }
