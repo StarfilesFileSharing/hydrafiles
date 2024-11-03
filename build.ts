9d663be6207d9325a8056805b6bf949e1cb0da79
Parsa Yazdani (Quix) <parsa@yazdani.au>
2024-11-03 18:36:04 +1100

    minify and tree shake builds

--- build.ts
+++ build.ts
@@ -1,6 +1,18 @@
 import * as esbuild from "npm:esbuild";
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader";
 
-console.log(await esbuild.build({ plugins: [...denoPlugins()], entryPoints: ["./src/hydrafiles.ts"], outfile: "./build/hydrafiles-web.esm.js", bundle: true, format: "esm", platform: "browser", sourcemap: true }));
+console.log(
+  await esbuild.build({
+    plugins: [...denoPlugins()],
+    entryPoints: ["./src/hydrafiles.ts"],
+    outfile: "./build/hydrafiles-web.esm.js",
+    bundle: true,
+    format: "esm",
+    platform: "browser",
+    sourcemap: true,
+    minify: true,
+    treeShaking: true,
+  }),
+);
 
 esbuild.stop();
