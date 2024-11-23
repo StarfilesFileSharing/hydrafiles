71cabb9a08ca05707e37226ad5435bf36cf4ac80
Parsa Yazdani (Quix) <parsa@yazdani.au>
2024-11-23 19:22:23 +1100

    Refactor RPC

--- src/services/services.ts
+++ src/services/services.ts
@@ -34,75 +34,75 @@ export default class Services {
 		return hostname;
 	}
 
-	public async fetch(req: Request, headers: Headers): Promise<Response> { // TODO: Refactor this
+	public async fetch(req: Request): Promise<Response> { // TODO: Refactor this
 		const now = Date.now();
 		const url = new URL(req.url);
-		const hostname = url.pathname.split("/")[2].toUpperCase();
+		const hostname = encodeBase32(url.pathname.split("/")[2]).toUpperCase();
 		console.log(`Hostname: ${hostname} Received Request`);
 
 		if (hostname in this.ownedServices) {
 			console.log(`Hostname: ${hostname} Serving response`);
-			return this.ownedServices[hostname].fetch(req, headers);
-		} else {
-			if (this.processingRequests.has(hostname)) {
-				if (Services._client.config.logLevel === "verbose") console.log(`  ${hostname}  Waiting for existing request with same hostname`);
-				await this.processingRequests.get(hostname);
-			}
-			const reqKey = req.url;
-			if (reqKey in this.cachedResponses) {
-				const cachedEntry = this.cachedResponses[reqKey];
-				if (now - cachedEntry.timestamp > 60000) {
-					delete this.cachedResponses[reqKey];
-				}
-				console.log(`Hostname: ${hostname} Serving response from cache`);
-				return new Response(this.cachedResponses[reqKey].body, { headers: this.cachedResponses[reqKey].headers });
-			}
-
-			const responses = Services._client.rpcClient.fetch(req.url);
-
-			const processingRequest = new Promise<Response | ErrorRequestFailed>((resolve, reject) => {
-				(async () => {
-					await Promise.all(responses.map(async (res) => {
-						try {
-							const response = await res;
-							if (response instanceof Error) return response;
-							if (!response.ok) return;
-							const body = await response.text();
-							const signature = response.headers.get("hydra-signature") as EthAddress | null;
-							if (signature !== null && await Services._client.filesWallet.verifyMessage(body, signature, new TextDecoder().decode(decodeBase32(hostname)) as EthAddress)) resolve(new Response(body, { headers: response.headers }));
-						} catch (e) {
-							const err = e as Error;
-							if (err.message !== "Hostname not found") console.error(e);
-						}
-					}));
-					reject(new Error("Hostname not found"));
-				})();
-			});
-
-			this.processingRequests.set(hostname, processingRequest);
-
-			let response: Response | ErrorRequestFailed | undefined;
-			try {
-				response = await processingRequest;
-			} catch (e) {
-				const err = e as Error;
-				if (err.message === "Hostname not found") {
-					console.log(`Hostname: ${hostname} Not found`);
-					return new Response("Hostname not found", { headers, status: 404 });
-				} else {
-					console.log(err.message);
-					throw err;
-				}
-			} finally {
-				this.processingRequests.delete(hostname);
-			}
-
-			if (response instanceof ErrorRequestFailed) throw response;
-
-			console.log(`Hostname: ${hostname} Mirroring response`);
-			const res = { body: await response.text(), headers: response.headers };
-			this.cachedResponses[reqKey] = { ...res, timestamp: Date.now() };
-			return new Response(res.body, { headers: res.headers });
+			return this.ownedServices[hostname].fetch(req, req.headers);
 		}
+
+		if (this.processingRequests.has(hostname)) {
+			if (Services._client.config.logLevel === "verbose") console.log(`  ${hostname}  Waiting for existing request with same hostname`);
+			await this.processingRequests.get(hostname);
+		}
+		const reqKey = req.url;
+		if (reqKey in this.cachedResponses) {
+			const cachedEntry = this.cachedResponses[reqKey];
+			if (now - cachedEntry.timestamp > 60000) {
+				delete this.cachedResponses[reqKey];
+			}
+			console.log(`Hostname: ${hostname} Serving response from cache`);
+			return new Response(this.cachedResponses[reqKey].body, { headers: this.cachedResponses[reqKey].headers });
+		}
+
+		const responses = await Services._client.rpcClient.fetch(req.url, { headers: req.headers });
+
+		const processingRequest = new Promise<Response | ErrorRequestFailed>((resolve, reject) => {
+			(async () => {
+				await Promise.all(responses.map(async (res) => {
+					try {
+						const response = await res;
+						if (response instanceof Error) return response;
+						if (!response.ok) return;
+						const body = await response.text();
+						const signature = response.headers.get("hydra-signature") as EthAddress | null;
+						if (signature !== null && await Services._client.filesWallet.verifyMessage(body, signature, new TextDecoder().decode(decodeBase32(hostname)) as EthAddress)) resolve(new Response(body, { headers: response.headers }));
+					} catch (e) {
+						const err = e as Error;
+						if (err.message !== "Hostname not found") console.error(e);
+					}
+				}));
+				reject(new Error("Hostname not found"));
+			})();
+		});
+
+		this.processingRequests.set(hostname, processingRequest);
+
+		let response: Response | ErrorRequestFailed | undefined;
+		try {
+			response = await processingRequest;
+		} catch (e) {
+			const err = e as Error;
+			if (err.message === "Hostname not found") {
+				console.log(`Hostname: ${hostname} Not found`);
+				return new Response("Hostname not found", { headers: req.headers, status: 404 });
+			} else {
+				console.log(err.message);
+				throw err;
+			}
+		} finally {
+			this.processingRequests.delete(hostname);
+		}
+
+		if (response instanceof ErrorRequestFailed) throw response;
+
+		console.log(`Hostname: ${hostname} Mirroring response`);
+		const res = { body: await response.text(), headers: response.headers };
+		this.cachedResponses[reqKey] = { ...res, timestamp: Date.now() };
+		return new Response(res.body, { headers: res.headers });
 	}
 }
