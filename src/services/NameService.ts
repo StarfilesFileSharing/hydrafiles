0ded49fc94779bb5bcfe3a86968aebdae9b19e66
Parsa Yazdani (Quix) <parsa@yazdani.au>
2024-11-24 00:31:55 +1100

    smart contracts

--- src/services/NameService.ts
+++ src/services/NameService.ts
@@ -1,14 +1,16 @@
+import { Sha256 } from "./../utils.ts";
 import Wallet, { EthAddress } from "../wallet.ts";
 import Hydrafiles from "../hydrafiles.ts";
 import Database from "../database.ts";
+import { encodeHex } from "jsr:@std/encoding/hex";
 
 const blockModel = {
 	tableName: "block",
 	columns: {
 		prev: { type: "TEXT" as const, primary: true },
-		address: { type: "TEXT" as const },
+		content: { type: "TEXT" as const, isNullable: true },
+		id: { type: "TEXT" as const, isNullable: true },
 		nonce: { type: "INTEGER" as const },
-		signature: { type: "TEXT" as const },
 		name: { type: "TEXT" as const },
 		createdAt: { type: "DATETIME" as const, default: "CURRENT_TIMESTAMP" },
 		updatedAt: { type: "DATETIME" as const, default: "CURRENT_TIMESTAMP" },
@@ -17,35 +19,31 @@ const blockModel = {
 
 interface BlockAttributes {
 	prev: string;
-	address: string;
 	nonce: number;
-	signature: string;
 	name: string;
+	content: string;
+	id: string;
 }
 
 export class Block implements BlockAttributes {
-	prev: EthAddress;
-	address: EthAddress;
+	prev: EthAddress | Sha256;
 	nonce: number;
-	signature: EthAddress;
 	name: string;
+	content: EthAddress | string;
+	id: EthAddress | Sha256;
 
-	constructor(address: EthAddress, nonce: number, prev: EthAddress, signature: EthAddress, name: string) {
-		this.address = address;
+	constructor(content: EthAddress | string, id: EthAddress | Sha256, nonce: number, prev: EthAddress | Sha256, name: string) {
+		this.id = id;
+		this.content = content;
 		this.nonce = nonce;
 		this.prev = prev;
-		this.signature = signature;
 		this.name = name;
-	}
-
-	toString(): string {
-		return `${this.address}-${this.nonce}-${this.prev}`;
 	}
 }
 
 export default class BlockchainNameService {
 	static _client: Hydrafiles;
-	blocks: Block[] = [new Block("0x0000000000000000000000000000000000000000", 0, "0x0000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000", "Genesis")];
+	blocks: Block[] = [new Block("0x0", "0x0", 0, "0x0", "Genesis")];
 	db: Database<typeof blockModel>;
 
 	private constructor(db: Database<typeof blockModel>) {
@@ -59,31 +57,52 @@ export default class BlockchainNameService {
 		const blocks = await db.select();
 		for (let i = 0; i < blocks.length; i++) {
 			const block = blocks[i];
-			blockchainNameService.blocks.push(new Block(block.address as EthAddress, block.nonce, block.prev as EthAddress, block.signature as EthAddress, block.name));
+			blockchainNameService.blocks.push(new Block(block.content, block.id as EthAddress | Sha256, block.nonce, block.prev as EthAddress, block.name));
 		}
 
 		return blockchainNameService;
 	}
 
-	async createBlock(wallet: Wallet, name: string): Promise<void> {
+	async createBlock(data: { wallet: Wallet } | { script: string }, name: string): Promise<void> {
 		const prev = this.blocks[this.blocks.length - 1];
 
-		let signature;
-		for (let i = 0; true; i++) {
-			signature = await wallet.signMessage(`${wallet.address()}-${i}-${prev.signature}`);
-			if (signature.startsWith("0x00")) break;
-			if (i % 100 === 0) console.log(`${i} attempts to find block`);
+		let nonce;
+		let id;
+		let content;
+		if ("wallet" in data) {
+			for (nonce = 0; true; nonce++) {
+				id = await data.wallet.signMessage(`${data.wallet.address()}-${nonce}-${prev.id}`);
+				if (id.startsWith("0x00")) {
+					content = data.wallet.address();
+					break;
+				}
+				if (nonce % 100 === 0) console.log(`${nonce} attempts to find block`);
+			}
+		} else {
+			for (nonce = 0; true; nonce++) {
+				id = encodeHex(await crypto.subtle.digest("SHA-256", new TextEncoder().encode(`${data.script}-${nonce}-${prev.id}`))) as Sha256;
+				if (id.startsWith("00")) {
+					content = data.script;
+					break;
+				}
+				if (nonce % 100 === 0) console.log(`${nonce} attempts to find block`);
+			}
 		}
-		const block = new Block(wallet.address(), 0, prev.signature, signature, name);
+		const block = new Block(content, id, nonce, prev.id, name);
+
 		this.db.insert(block);
 		this.blocks.push(block);
 	}
 
-	addBlock(block: Block): boolean {
+	async addBlock(block: Block): Promise<boolean> {
 		const prev = this.blocks[this.blocks.length - 1];
 
-		if (block.prev !== prev.signature) return false; // TODO: Longest chain
-		if (!BlockchainNameService._client.filesWallet.verifyMessage(block.toString(), block.signature, block.address)) return false;
+		if (block.prev !== prev.id) return false; // TODO: Longest chain
+		if (block.id.startsWith("0x")) {
+			if (!BlockchainNameService._client.filesWallet.verifyMessage(block.toString(), block.id as EthAddress, block.content as EthAddress)) return false;
+		} else {
+			if (encodeHex(await crypto.subtle.digest("SHA-256", new TextEncoder().encode(`${block.content}-${block.nonce}-${block.prev}`))) !== block.id) return false;
+		}
 
 		this.db.insert(block);
 		this.blocks.push(block);
@@ -100,8 +119,8 @@ export default class BlockchainNameService {
 			try {
 				const blocks = JSON.parse(data) as BlockAttributes[];
 				for (let j = 0; j < blocks.length; j++) {
-					if (this.blocks[j].signature === blocks[j].signature) continue;
-					const block = new Block(blocks[j].address as EthAddress, blocks[j].nonce, blocks[j].prev as EthAddress, blocks[j].signature as EthAddress, blocks[j].name);
+					if (this.blocks[j].id === blocks[j].id) continue;
+					const block = new Block(blocks[j].content, blocks[j].id as EthAddress | Sha256, blocks[j].nonce, blocks[j].prev as EthAddress, blocks[j].name);
 					if (!this.addBlock(block)) break;
 					console.log(`Blocks:   Received block`);
 				}
