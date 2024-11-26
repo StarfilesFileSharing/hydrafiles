78c1a751dc98a8a99ff12bfd489225b2270bbd8b
Parsa Yazdani (Quix) <parsa@yazdani.au>
2024-11-27 04:18:08 +1100

    Path join

--- src/filesystem/filesystem.ts
+++ src/filesystem/filesystem.ts
@@ -1,3 +1,4 @@
+import { join } from "https://deno.land/std@0.224.0/path/join.ts";
 import { ErrorNotInitialised } from "../errors.ts";
 import type Hydrafiles from "../hydrafiles.ts";
 import DirectoryHandleFileSystem from "./DirectoryHandleFileSystem.ts";
@@ -13,11 +14,11 @@ export default class FileSystem {
 		if (this.fs instanceof IndexedDBFileSystem) this.fs.dbPromise = this.fs.initDB();
 		this._client = client;
 	}
-	exists = async (path: string) => this.fs ? await this.fs.exists(`${this._client.config.baseDir}${path}`) : new ErrorNotInitialised();
-	mkdir = async (path: `${string}/`) => this.fs ? await this.fs.mkdir(`${this._client.config.baseDir}${path}`) : new ErrorNotInitialised();
-	readDir = async (path: `${string}/`) => this.fs ? await this.fs.readDir(`${this._client.config.baseDir}${path}`) : new ErrorNotInitialised();
-	readFile = async (path: string) => this.fs ? await this.fs.readFile(`${this._client.config.baseDir}${path}`) : new ErrorNotInitialised();
-	writeFile = async (path: string, data: Uint8Array) => this.fs ? await this.fs.writeFile(`${this._client.config.baseDir}${path}`, data) : new ErrorNotInitialised();
-	getFileSize = async (path: string) => this.fs ? await this.fs.getFileSize(`${this._client.config.baseDir}${path}`) : new ErrorNotInitialised();
-	remove = async (path: string) => this.fs ? await this.fs.remove(`${this._client.config.baseDir}${path}`) : new ErrorNotInitialised();
+	exists = async (path: string) => this.fs ? await this.fs.exists(`${join(this._client.config.baseDir, path)}`) : new ErrorNotInitialised();
+	mkdir = async (path: `${string}/`) => this.fs ? await this.fs.mkdir(`${join(this._client.config.baseDir, path)}/`) : new ErrorNotInitialised();
+	readDir = async (path: `${string}/`) => this.fs ? await this.fs.readDir(`${join(this._client.config.baseDir, path)}/`) : new ErrorNotInitialised();
+	readFile = async (path: string) => this.fs ? await this.fs.readFile(`${join(this._client.config.baseDir, path)}`) : new ErrorNotInitialised();
+	writeFile = async (path: string, data: Uint8Array) => this.fs ? await this.fs.writeFile(`${join(this._client.config.baseDir, path)}`, data) : new ErrorNotInitialised();
+	getFileSize = async (path: string) => this.fs ? await this.fs.getFileSize(`${join(this._client.config.baseDir, path)}`) : new ErrorNotInitialised();
+	remove = async (path: string) => this.fs ? await this.fs.remove(`${join(this._client.config.baseDir, path)}`) : new ErrorNotInitialised();
 }
