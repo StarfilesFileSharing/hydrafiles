cbe392ce049cd15f2a85c181fa6d504fbbb057d3
Parsa Yazdani (Quix) <parsa@yazdani.au>
2025-01-14 23:43:57 +1100

    test

--- src/rpc/routes.ts
+++ src/rpc/routes.ts
@@ -86,6 +86,8 @@ router.set("/peers", (_, client) => {
 					if (key.startsWith("_")) continue;
 					outputPeer[key as keyof PeerAttributes] = value;
 				}
+				Utils.log(outputPeer);
+				JSON.stringify(outputPeer);
 				return outputPeer;
 			}),
 		),
